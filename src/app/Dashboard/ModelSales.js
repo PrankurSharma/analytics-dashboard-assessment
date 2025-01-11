9ef98b0c8389f06accd8dd775db49ced9bf7bd30
PrankurSharma <66331250+PrankurSharma@users.noreply.github.com>
2025-01-12 05:23:29 +0530

    Update ModelSales.js

--- src/app/Dashboard/ModelSales.js
+++ src/app/Dashboard/ModelSales.js
@@ -6,9 +6,10 @@ import Card from "../UI/Card";
 export default function ModelSales({ data }) {
     const chartData = calculateYearlySales(data);
     return (
-        <Col sm={24} md={10} lg={10}>
+        <Col xs={24} sm={24} md={10} lg={10}>
             <Card title={"Yearly Sales"} loading={!data} style={{
                 height: "100%",
+                minHeight: "200px",
                 display: "flex",
                 flexDirection: "column"
             }} styles={{
